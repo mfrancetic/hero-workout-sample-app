5258787ee5ae0ab3b10c9ae0fbf229e168979765
Maja Francetic <francetic.maja@gmail.com>
2021-01-19 10:23:04 +0100

    Create LoginFragment; navigate from SplashFragment to LoginFragment

---
+++ app/src/main/java/com/mfrancetic/heroworkoutsampleapp/screens/login/LoginViewModel.kt
@@ -0,0 +1,7 @@
+package com.mfrancetic.heroworkoutsampleapp.screens.login
+
+import androidx.lifecycle.ViewModel
+
+class LoginViewModel : ViewModel() {
+
+}
--- app/src/main/java/com/mfrancetic/heroworkoutsampleapp/screens/splash/SplashFragment.kt
+++ app/src/main/java/com/mfrancetic/heroworkoutsampleapp/screens/splash/SplashFragment.kt
@@ -5,6 +5,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.mfrancetic.heroworkoutsampleapp.R
 import com.mfrancetic.heroworkoutsampleapp.databinding.SplashFragmentBinding
 
 class SplashFragment : Fragment() {
@@ -18,5 +20,15 @@ class SplashFragment : Fragment() {
 
         binding = SplashFragmentBinding.inflate(inflater)
         return binding.root
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        navigateToLoginFragment()
+    }
+
+    private fun navigateToLoginFragment() {
+        findNavController().navigate(R.id.action_splashFragment_to_loginFragment)
     }
 }
