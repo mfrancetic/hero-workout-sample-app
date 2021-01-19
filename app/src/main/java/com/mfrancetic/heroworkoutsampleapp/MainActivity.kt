516c495e4836434c3bf28223c4d5b276239f7d1d
Maja Francetic <francetic.maja@gmail.com>
2021-01-19 16:59:57 +0100

    Delete unused import statement

--- app/src/main/java/com/mfrancetic/heroworkoutsampleapp/MainActivity.kt
+++ app/src/main/java/com/mfrancetic/heroworkoutsampleapp/MainActivity.kt
@@ -6,8 +6,6 @@ import androidx.databinding.DataBindingUtil
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.NavigationUI
-import androidx.navigation.ui.setupActionBarWithNavController
 import com.mfrancetic.heroworkoutsampleapp.databinding.ActivityMainBinding
 
 private lateinit var binding: ActivityMainBinding
