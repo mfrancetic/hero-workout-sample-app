4ef030bcfc58408900eda5acf754d5e72d4cf7e3
Maja Francetic <francetic.maja@gmail.com>
2021-01-19 09:43:43 +0100

    Implement the navigation and data binding libraries; create SplashFragment as a start destination of the NavGraph

--- app/src/main/java/com/mfrancetic/heroworkoutsampleapp/MainActivity.kt
+++ app/src/main/java/com/mfrancetic/heroworkoutsampleapp/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.mfrancetic.heroworkoutsampleapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import com.mfrancetic.heroworkoutsampleapp.databinding.ActivityMainBinding
+
+private lateinit var binding: ActivityMainBinding
+private lateinit var navController: NavController
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        setupNavControler()
+    }
+
+    private fun setupNavControler() {
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
     }
 }
---
+++ app/src/main/java/com/mfrancetic/heroworkoutsampleapp/screens/splash/SplashFragment.kt
@@ -0,0 +1,22 @@
+package com.mfrancetic.heroworkoutsampleapp.screens.splash
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.mfrancetic.heroworkoutsampleapp.databinding.SplashFragmentBinding
+
+class SplashFragment : Fragment() {
+
+    private lateinit var binding: SplashFragmentBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+
+        binding = SplashFragmentBinding.inflate(inflater)
+        return binding.root
+    }
+}
