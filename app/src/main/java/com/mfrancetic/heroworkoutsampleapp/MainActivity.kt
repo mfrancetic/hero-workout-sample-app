c964bb3470ad3ce0cc682206051bc2423774ed39
Maja Francetic <francetic.maja@gmail.com>
2021-01-19 12:46:45 +0100

    Setup topLevelDestinations and AppBarConfiguration for the MainActivity; disable going back to the SplashFragment

--- app/src/main/java/com/mfrancetic/heroworkoutsampleapp/MainActivity.kt
+++ app/src/main/java/com/mfrancetic/heroworkoutsampleapp/MainActivity.kt
@@ -5,10 +5,15 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.mfrancetic.heroworkoutsampleapp.databinding.ActivityMainBinding
 
 private lateinit var binding: ActivityMainBinding
 private lateinit var navController: NavController
+private lateinit var appBarConfiguration: AppBarConfiguration
+private lateinit var topLevelDestinations: Set<Int>
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,5 +28,24 @@ class MainActivity : AppCompatActivity() {
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         navController = navHostFragment.navController
+
+        topLevelDestinations = setOf(R.id.loginFragment)
+        appBarConfiguration = AppBarConfiguration.Builder(topLevelDestinations)
+            .build()
+
+        setupActionBarWithNavController(navController, appBarConfiguration)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp()
+                || super.onSupportNavigateUp()
+    }
+
+    override fun onBackPressed() {
+        if (topLevelDestinations.contains(navController.currentDestination?.id)) {
+            finish()
+        } else {
+            onSupportNavigateUp()
+        }
     }
 }
